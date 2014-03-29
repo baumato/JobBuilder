5373759956f25597599cf4b3ef4a87771267d383
tobbaumann <tobbaumann@gmail.com>
2014-03-29 16:46:38 +0100

    initial commit

---
+++ org.tobbaumann.jobs.builder.examples/src/org/tobbaumann/jobs/builder/examples/Activator.java
@@ -0,0 +1,39 @@
+package org.tobbaumann.jobs.builder.examples;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends AbstractUIPlugin {
+
+	private static final String PLUGIN_ID = "org.tobbaumann.jobs.builder.examples";
+
+	private static Activator plugin;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+}
---
+++ org.tobbaumann.jobs.builder.examples/src/org/tobbaumann/jobs/builder/examples/ApplicationActionBarAdvisor.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2014 tobbaumann.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     tobbaumann - initial API and implementation
+ ******************************************************************************/
+package org.tobbaumann.jobs.builder.examples;
+
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+		super(configurer);
+	}
+
+}
